0ebb013a2ea3eb99be100a9b627478d7e40590ed
Fred Hornsey <fred@hornsey.us>
2020-02-05 20:46:11 -0600

    Tweaked subscriber.py

--- tests/basic_test/subscriber.py
+++ tests/basic_test/subscriber.py
@@ -1,25 +1,26 @@
 import sys
 from datetime import timedelta
 
-from pyopendds import init_opendds, DomainParticipant, StatusKind, PyOpenDDS_Error
-import pybasic.basic
+from pyopendds import \
+    init_opendds, DomainParticipant, StatusKind, PyOpenDDS_Error
+from pybasic.basic import Reading
 
 if __name__ == "__main__":
     try:
-        # Initialize OpenDDS and Create DDS Objects
+        # Initialize OpenDDS and Create DDS Entities
         init_opendds(opendds_debug_level=1)
-        part = DomainParticipant(34)
-        topic = part.create_topic('Readings', pybasic.basic.Reading)
-        sub = part.create_subscriber()
-        dr = sub.create_datareader(topic)
+        domain = DomainParticipant(34)
+        topic = domain.create_topic('Readings', Reading)
+        subscriber = domain.create_subscriber()
+        reader = subscriber.create_datareader(topic)
 
         # Wait for Publisher to Connect
         print('Waiting for Publisher...')
-        dr.wait_for(StatusKind.SUBSCRIPTION_MATCHED, timedelta(seconds=15))
+        reader.wait_for(StatusKind.SUBSCRIPTION_MATCHED, timedelta(seconds=5))
         print('Found Publisher!')
 
         # Read and Print Sample
-        print(dr.take_next_sample())
+        print(reader.take_next_sample())
 
         print('Done!')
 
