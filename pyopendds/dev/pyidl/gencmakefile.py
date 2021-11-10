1b31c7f9690a3e3c5e5914cfedc4db3139c0877e
Andrea Ruffino <andrea.ruffino@hotmail.fr>
2021-11-10 17:11:48 +0100

    Reformat CMakefile generated by pyidl

--- pyopendds/dev/pyidl/gencmakefile.py
+++ pyopendds/dev/pyidl/gencmakefile.py
@@ -10,16 +10,21 @@ def gen_cmakelist(target_name: str,
 
     statement_1 = ""
     for idx, _ in enumerate(include_dirs):
-        statement_1 += f'target_include_directories({target_name}_idl PUBLIC "${{inc_dir_{idx}}}")\n'
+        statement_1 += f'\ntarget_include_directories({target_name}_idl PUBLIC "${{inc_dir_{idx}}}")'
 
     statement_3 = ""
     for idl_file in idl_files:
-        statement_3 += f' {idl_file}'
-    statement_3 = f'PUBLIC{statement_3}'
+        statement_3 += f'\n    {idl_file}'
 
     statement_4 = ""
     for idx, _ in enumerate(include_dirs):
-        statement_4 += f' -I${{inc_dir_{idx}}}'
+        statement_4 += f'\n    -I${{inc_dir_{idx}}}'
+
+    statement_5 = ""
+    if venv_path:
+        statement_5 += f'set_target_properties({target_name}_idl PROPERTIES\n'
+        statement_5 += f'    LIBRARY_OUTPUT_DIRECTORY "{venv_path}/lib/pyidl/{target_name}"\n'
+        statement_5 += ')'
 
     statement_5 = ""
     if venv_path:
@@ -36,17 +41,15 @@ list(APPEND CMAKE_MODULE_PATH "$ENV{{DDS_ROOT}}/cmake")
 find_package(OpenDDS REQUIRED)
 {statement_0}
 add_library({target_name}_idl SHARED)
-target_include_directories({target_name}_idl PUBLIC "${{CMAKE_CURRENT_SOURCE_DIR}}/build")
-{statement_1}
-set_target_properties({target_name}_idl PROPERTIES
-    LIBRARY_OUTPUT_DIRECTORY "{venv_path}/lib/{target_name}"
-)
+target_include_directories({target_name}_idl PUBLIC "${{CMAKE_CURRENT_SOURCE_DIR}}/build"){statement_1}
+{statement_5}
+
 if(${{CPP11_IDL}})
     set(opendds_idl_mapping_option "-Lc++11")
 endif()
 
 set(OPENDDS_FILENAME_ONLY_INCLUDES ON)
-OPENDDS_TARGET_SOURCES({target_name}_idl {statement_3}
+OPENDDS_TARGET_SOURCES({target_name}_idl PUBLIC{statement_3}
     OPENDDS_IDL_OPTIONS "-Gitl" "${{opendds_idl_mapping_option}}"{statement_4}
 )
 
@@ -56,5 +59,8 @@ export(
     FILE "${{CMAKE_CURRENT_BINARY_DIR}}/{target_name}_idlConfig.cmake"
 )
 
-target_include_directories({target_name}_idl PUBLIC "{pyopendds_ldir}")
+target_include_directories({target_name}_idl PUBLIC
+    "{pyopendds_ldir}"
+)
+
 """
