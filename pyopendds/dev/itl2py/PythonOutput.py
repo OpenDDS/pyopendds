323eb8c141b168a0bc9eb89e7dda9f5eefbf0221
Paul Rousse <paul.rousse@airbus.com>
2022-01-11 18:34:42 +0100

    code readability fix

--- pyopendds/dev/itl2py/PythonOutput.py
+++ pyopendds/dev/itl2py/PythonOutput.py
@@ -62,8 +62,6 @@ class PythonOutput(Output):
     def get_python_type_string(self, field_type):
         if isinstance(field_type, PrimitiveType):
             return self.primitive_types[field_type.kind][0]
-        # elif self.is_local_type(field_type):
-        #     return field_type.local_name()
         else:
             return field_type.local_name()
 
@@ -80,9 +78,9 @@ class PythonOutput(Output):
                 subfield_type = field_type.base_type
                 subtype_name = self.get_python_type_string(subfield_type)
                 if isinstance(subfield_type, PrimitiveType):
-                    return 'field(default_factory=lambda :['+self.primitive_types[subfield_type.kind][1]+'])'
+                    return "field(default_factory=lambda :[{type}])".format(type=self.primitive_types[subfield_type.kind][1])
                 else : 
-                    return 'field(default_factory=lambda :['+subtype_name+'()])'
+                    return 'field(default_factory=lambda :[{type}()])'.format(type=subtype_name)
             elif isinstance(field_type, ArrayType):
                 return 'field(default_factory=list)'
             else:
