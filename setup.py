391f5a730eccc760f4d5b33340d212765faca2f2
sfaucherDF <simon.faucher.external@airbus.com>
2022-01-24 14:29:26 +0100

    Changed the setup.py to add the ext folder in the bdist build but not in te package During th 21/01/2021 meeting, changed have been performed to include the ext folder in the bdist build but not in the package. It's working.

--- setup.py
+++ setup.py
@@ -1,8 +1,32 @@
 from pathlib import Path
 from setuptools import setup, find_packages
-
+from glob import glob
 from pyopendds.dev.cmake import CMakeWrapperExtension, CMakeWrapperBuild
+import distutils.command.sdist
+import sys
+# class SdistCommand(distutils.command.sdist.sdist):
+#     def run(self):
+#         # 'filelist' contains the list of files that will make up the
+#         # manifest
+#         self.filelist = FileList()
 
+#         # Run sub commands
+#         for cmd_name in self.get_sub_commands():
+#             self.run_command(cmd_name)
+
+#         # Do whatever it takes to get the list of files to process
+#         # (process the manifest template, read an existing manifest,
+#         # whatever).  File list is accumulated in 'self.filelist'.
+#         self.get_file_list()
+#         # If user just wanted us to regenerate the manifest, stop now.
+#         if self.manifest_only:
+#             return
+
+#         # Otherwise, go ahead and create the source distribution tarball,
+#         # or zipfile, or whatever.
+#         self.make_distribution()
+
+ext_package = {'pyopendds': ['ext/*']}
 setup(
     packages=find_packages(),
     ext_modules=[CMakeWrapperExtension(
@@ -21,13 +45,19 @@ setup(
         ],
     },
     package_data={
+        'pyopendds':['ext/*'] if 'sdist' in sys.argv else [],
+        'pyopendds':['ext/*'] if 'bdist_rpm' in sys.argv else [],
         'pyopendds.dev': [
             'include/pyopendds/*',
         ],
         'pyopendds.dev.itl2py': [
-            'templates/*',
+            'templates/*', 
         ],
+        
     },
+    
+
+    
     install_requires=[
         'Jinja2'
     ],
